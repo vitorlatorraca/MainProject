e8513eeeecdd7a15f6962e3074e09fcca47756a3
vitorlatorraca <vitorlatorraca5@gmail.com>
2024-07-16 00:03:44 -0400

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,41 @@
+import Image from 'next/image'; // Import the Image component
+import { Button } from "@/components/ui/button";
+import PatientForm from '@/components/ui/forms/PatientForm';
+import Link from 'next/link';
 
 export default function Home() {
-return (
-  <div>
-    <h1 className=" text-white text-3xl underline">Home</h1>
-  </div>
-)
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit" // Fixed className typo
+          />
+
+          <PatientForm/>
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+           © 2024 CarePulse
+           </p>
+           <Link href="/?admin=true" className="text-green-500">
+            Admin 
+           </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image 
+      src="/assets/images/onboarding-img.png"
+      height={1000}
+      width={1000}
+      alt="patient"
+      className="side-img max-w-[50%]"
+      />
+    </div>
+  );
 }
