26caf06ab6f0a8461852ecd4b45e44ac75d970b2
vitorlatorraca <vitorlatorraca5@gmail.com>
2024-07-15 21:16:29 -0400

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
+import { cn } from '/@lib/utils';
+
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"], 
-  weights: ['300', '400', '500', '600', '700'],
+  weight: ['300', '400', '500', '600', '700'],
   variable : '--font-sans'
   });
 
@@ -20,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn}>{children}</body>
     </html>
   );
 }
