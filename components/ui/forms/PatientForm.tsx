809e46291997aeb24b20be001ca20d7d5b0910ae
vitorlatorraca <vitorlatorraca5@gmail.com>
2024-07-31 22:51:25 -0400

    new

--- components/ui/forms/PatientForm.tsx
+++ components/ui/forms/PatientForm.tsx
@@ -21,8 +21,8 @@ const PatientForm = () => {
  
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values
-    // ✅ This will be type-safe and validated
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
     console.log(values)
   }
   return (
