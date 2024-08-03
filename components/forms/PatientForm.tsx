2261299abfd791d5065aa1dbf36849309b5e538a
vitorlatorraca <vitorlatorraca5@gmail.com>
2024-08-03 15:08:26 -0400

    new

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,6 +5,7 @@ import { Form } from "@/components/ui/form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import CustomFormField from "../CustomFormField"
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -29,11 +30,13 @@ const PatientForm = () => {
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
 
         <section className="mb-12 space-y-4">
-
           <h1 className="header"> Hi there </h1>
           <p className="text-dark-700"> Schedule your first appointment</p>
-
         </section>
+
+        <CustomFormField
+        control={form.control}
+        />
 
         <Button type="submit">Submit</Button>
       </form>
