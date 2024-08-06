4e160472b6661ed4515127be087d54b43d79b811
vitorlatorraca <vitorlatorraca5@gmail.com>
2024-08-05 22:50:04 -0400

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
-import Image from 'next/image'; // Import the Image component
-import { Button } from "@/components/ui/button";
-import PatientForm from '@/components/forms/PatientForm';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+import { PatientForm } from "@/components/forms/PatientForm";
+import { PasskeyModal } from "@/components/PasskeyModal";
+
+const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams?.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification / PasskeyModal*/}
-      
+      {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -15,29 +18,31 @@ export default function Home() {
             height={1000}
             width={1000}
             alt="patient"
-            className="mb-12 h-10 w-fit" // Fixed className typo
+            className="mb-12 h-10 w-fit"
           />
 
-          <PatientForm/>
+          <PatientForm />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-           © 2024 CarePulse
-           </p>
-           <Link href="/?admin=true" className="text-green-500">
-            Admin 
-           </Link>
+              © 2024 CarePluse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
           </div>
         </div>
       </section>
 
-      <Image 
-      src="/assets/images/onboarding-img.png"
-      height={1000}
-      width={1000}
-      alt="patient"
-      className="side-img max-w-[50%]"
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
       />
     </div>
   );
-}
+};
+
+export default Home;
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,63 +1,92 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { Form } from "@/components/ui/form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import CustomFormField from "../CustomFormField"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-export enum FormFieldType {
-  INPUT = 'input',
-  TEXTAREA = 'textarea',
-  PHONE_INPUT = 'phoneInput',
-  CHECKBOX = 'checkbox',
-  DATE_PICKER = 'datePicker',
-  SELECT = 'select',
-  SKELETON = 'skeleton', 
-}
+import { Form } from "@/components/ui/form";
+import { createUser } from "@/lib/actions/patient.actions";
+import { UserFormValidation } from "@/lib/validation";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+import "react-phone-number-input/style.css";
+import CustomFormField, { FormFieldType } from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
 
-const PatientForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+export const PatientForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
     defaultValues: {
-      username: "",
+      name: "",
+      email: "",
+      phone: "",
     },
-  })
- 
+  });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
-  }
+  const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+    setIsLoading(true);
+
+    try {
+      const user = {
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+      };
+
+      const newUser = await createUser(user);
+
+      if (newUser) {
+        router.push(`/patients/${newUser.$id}/register`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    setIsLoading(false);
+  };
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
         <section className="mb-12 space-y-4">
-          <h1 className="header"> Hi there </h1>
-          <p className="text-dark-700"> Schedule your first appointment</p>
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Get started with appointments.</p>
         </section>
 
         <CustomFormField
-        fieldType={FormFieldType.INPUT}
-        control={control}
-        name="name"
-        label="Full name"
-        placeholder="John Doe"
-        iconSrc="/assets/icons/user.svg"
-        iconAlt="user"
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="John Doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
         />
 
-        <Button type="submit">Submit</Button>
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="johndoe@gmail.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(555) 123-4567"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
-  )
-}
-
-export default PatientForm
+  );
+};
